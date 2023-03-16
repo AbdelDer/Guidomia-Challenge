119a41f62cec8b657f576f3b461f102ac8c824d7
DERAOUI Abdelmaoula <mac@MacBook-Pro-de-mac.local>
2023-03-16 17:28:19 +0100

    test the getAllCarsUseCase

---
+++ app/src/test/java/com/kader/guidomia_challenge/use_case/GetAllCarsUseCaseTest.kt
@@ -0,0 +1,70 @@
+package com.kader.guidomia_challenge.use_case
+
+
+import com.kader.guidomia_challenge.common.Resource
+import com.kader.guidomia_challenge.doamin.model.Car
+import com.kader.guidomia_challenge.doamin.repository.CarRepository
+import com.kader.guidomia_challenge.doamin.use_case.GetAllCarsUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetAllCarsUseCaseTest {
+
+    @Mock
+    lateinit var carRepository: CarRepository
+
+    @Test
+    fun `invoke with empty parameters calls getAllCars`(): Unit = runBlocking{
+        // Given
+        val getAllCarsUseCase = GetAllCarsUseCase(carRepository)
+        val expectedCars = listOf(
+            Car(
+                consList = listOf(
+                    "Bad direction"
+                ),
+                make = "Land Rover",
+                marketPrice = 125000.0,
+                model = "Range Rover",
+                prosList = listOf(
+                    "You can go everywhere",
+                    "Good sound system"
+                ),
+                rating = 3,
+                expanded = false,
+                image = null
+            ),
+            Car(
+                consList = listOf(
+                    "Bad direction"
+                ),
+                make = "Roadster",
+                marketPrice = 125000.0,
+                model = "Alpine",
+                prosList = listOf(
+                    "You can go everywhere",
+                    "Good sound system"
+                ),
+                rating = 3,
+                expanded = false,
+                image = null
+            ),
+        )
+        `when`(carRepository.getAllCars()).thenReturn(flowOf(Resource.Success(expectedCars)))
+
+        // When
+        val result = getAllCarsUseCase()
+
+        // Then
+        assertEquals(expectedCars, result.first().data)
+        verify(carRepository).getAllCars()
+    }
+}
