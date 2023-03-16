4c994047bcaddff4a4fc36cc43ae2fb014f4a3ec
DERAOUI Abdelmaoula <mac@MacBook-Pro-de-mac.local>
2023-03-16 09:14:21 +0100

    home fragment created

---
+++ app/src/main/java/com/kader/guidomia_challenge/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.kader.guidomia_challenge.presentation.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kader.guidomia_challenge.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
