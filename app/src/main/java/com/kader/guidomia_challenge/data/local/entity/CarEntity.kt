c318a38e7fb9da7aa2e8f5aeb5d6a7d6e26cd08d
DERAOUI Abdelmaoula <mac@MacBook-Pro-de-mac.local>
2023-03-17 15:11:24 +0100

    create car entity

---
+++ app/src/main/java/com/kader/guidomia_challenge/data/local/entity/CarEntity.kt
@@ -0,0 +1,32 @@
+package com.kader.guidomia_challenge.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kader.guidomia_challenge.doamin.model.Car
+import com.squareup.moshi.JsonClass
+
+
+@Entity
+@JsonClass(generateAdapter = true)
+data class CarEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val make: String,
+    val model: String,
+    val customerPrice: Double,
+    val marketPrice: Double,
+    val rating: Int,
+    val prosList: List<String>,
+    val consList: List<String>
+) {
+    fun toCar(): Car {
+        return Car(
+            consList = consList,
+            make = make,
+            marketPrice = marketPrice,
+            model = model,
+            prosList = prosList,
+            rating = rating,
+        )
+    }
+}
