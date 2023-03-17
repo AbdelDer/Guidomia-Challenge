3fc877a61a2f7f8d4954cfaefbac8936805b5512
DERAOUI Abdelmaoula <mac@MacBook-Pro-de-mac.local>
2023-03-17 16:05:48 +0100

    update carrepository

--- app/src/main/java/com/kader/guidomia_challenge/doamin/repository/CarRepository.kt
+++ app/src/main/java/com/kader/guidomia_challenge/doamin/repository/CarRepository.kt
@@ -2,6 +2,7 @@ package com.kader.guidomia_challenge.doamin.repository
 
 
 import com.kader.guidomia_challenge.common.Resource
+import com.kader.guidomia_challenge.data.local.entity.CarEntity
 import com.kader.guidomia_challenge.doamin.model.Car
 import kotlinx.coroutines.flow.Flow
 
@@ -11,4 +12,5 @@ interface CarRepository {
     fun getAllMakeExisting(): Flow<Resource<List<String>>>
     fun getAllModelsExisting(): Flow<Resource<List<String>>>
     fun getCarsByParameters(model: String, make: String): Flow<Resource<List<Car>>>
+    fun insertCar(car: CarEntity)
 }
